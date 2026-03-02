ad9ea066953eae640a8c99a8dab3ed1ef81279ca
Joe D'Alessandro <joe@wahilacreative.com>
2026-03-02 13:16:11 -0500

    day 1 part 1

--- src/adventofcode/y2024/day1.clj
+++ src/adventofcode/y2024/day1.clj
@@ -1,4 +1,22 @@
 (ns adventofcode.y2024.day1
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :refer [split-lines split]]))
 
-(def input (str/split-lines (slurp "inputs/2024/day1.txt")))
+(def input (split-lines (slurp "inputs/2024/day1.txt")))
+;(def input (split-lines (slurp "examples/2024/day1.txt")))
+
+(defn parse-day1 [lines] (map #(split % #"\s+") lines))
+(defn diff [parsed]
+  (let [pull-column (fn [col]
+                      (map #(Integer/parseInt %)
+                           (map #(nth % col) parsed)))]
+    (map abs
+         (map -
+              (sort (pull-column 0))
+              (sort (pull-column 1))))))
+(defn sum [diffs] (apply + diffs))
+
+(defn part1 [input]
+  (-> input
+      (parse-day1)
+      (diff)
+      (sum)))
