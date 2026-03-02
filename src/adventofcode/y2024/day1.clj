5926c43d79446d7a0d07988cfb3e0473773f767c
Joe D'Alessandro <joe@wahilacreative.com>
2026-03-02 14:02:10 -0500

    day 1 part 2

--- src/adventofcode/y2024/day1.clj
+++ src/adventofcode/y2024/day1.clj
@@ -2,21 +2,35 @@
   (:require [clojure.string :refer [split-lines split]]))
 
 (def input (split-lines (slurp "inputs/2024/day1.txt")))
-;(def input (split-lines (slurp "examples/2024/day1.txt")))
+(def example (split-lines (slurp "examples/2024/day1.txt")))
 
-(defn parse-day1 [lines] (map #(split % #"\s+") lines))
-(defn diff [parsed]
-  (let [pull-column (fn [col]
-                      (map #(Integer/parseInt %)
-                           (map #(nth % col) parsed)))]
+(defn parse [lines] (map #(split % #"\s+") lines))
+
+(defn pull-column [parsed col]
+  (map #(Integer/parseInt %)
+       (map #(nth % col) parsed)))
+
+(defn score [parsed] 
+  (let [reference (frequencies (pull-column parsed 1))]
+    (map #(* % (if (reference %) 
+                 (reference %) 0)) 
+         (pull-column parsed 0))))
+
+(defn diff [parsed] 
     (map abs
          (map -
-              (sort (pull-column 0))
-              (sort (pull-column 1))))))
+              (sort (pull-column parsed 0))
+              (sort (pull-column parsed 1)))))
 (defn sum [diffs] (apply + diffs))
 
-(defn part1 [input]
-  (-> input
-      (parse-day1)
+(defn part1 [data]
+  (-> data
+      (parse)
       (diff)
+      (sum)))
+
+(defn part2 [data]
+  (-> data
+      (parse)
+      (score)
       (sum)))
