c336dea25a9b04425a6bd51209e180c64207a938
Joe D'Alessandro <joe@thejoeshow.net>
2026-03-05 12:16:49 -0500

    day 2, part 1

--- src/adventofcode/y2024/day2.clj
+++ src/adventofcode/y2024/day2.clj
@@ -1,11 +1,36 @@
 (ns adventofcode.y2024.day2
   (:require [adventofcode.utils
              :refer [parse-space
+                     parse-numbers
                      get-input
                      get-example]]))
 
 (def input (get-input 2024 2))
 (def example (get-example 2024 2))
 
-(-> example
-    (parse-space))
+(defn pairs [lines]
+  (map (fn [line]
+         (partition 2 1 line))
+       lines))
+
+(defn diffs [lines]
+  (map (fn [line]
+         (map
+          #(apply - %)
+          line))
+       lines))
+
+(defn safe? [line]
+   (and (<= (apply max (map abs line)) 3)
+        (or
+         (every? #(> % 0) line)
+         (every? #(< % 0) line))))
+
+(->> input
+     (parse-space)
+     (parse-numbers)
+     (pairs)
+     (diffs)
+     (map safe?)
+     (filter true?)
+     (count))
