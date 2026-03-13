d9b054d6455922bf58699ed51f8711477762888e
Joe D'Alessandro <joe@wahilacreative.com>
2026-03-13 13:38:17 -0400

    outlining day 5

---
+++ src/adventofcode/y2024/day5.clj
@@ -0,0 +1,11 @@
+(ns adventofcode.y2024.day5
+  (:require [adventofcode.utils :refer [get-example
+                                        get-input]]))
+
+(def input (get-input 2024 5))
+(def example (get-example 2024 5))
+
+(defn part1 [data]
+  (->> data))
+
+(part1 example)
