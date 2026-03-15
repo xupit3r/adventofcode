940e384d5aa851196eb4244bb3da7dbb4c8db3bd
Joe D'Alessandro <joe@thejoeshow.net>
2026-03-15 12:57:52 -0400

    day 6 boilerplate

---
+++ src/adventofcode/y2024/day6.clj
@@ -0,0 +1,11 @@
+(ns adventofcode.y2024.day6
+  (:require [adventofcode.utils :refer [get-input
+                                        get-example]]))
+
+(def input (get-input 2024 6))
+(def example (get-example 2024 6))
+
+(defn part1 [data]
+  (->> data))
+
+(part1 example)
