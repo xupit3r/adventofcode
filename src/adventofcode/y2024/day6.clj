08ccd1a57732a988b84d0732fa1a37740b1ed687
Joe D'Alessandro <joe@thejoeshow.net>
2026-06-24 11:24:14 -0400

    day 6, part 1 (soooo many small fuck ups...)

--- src/adventofcode/y2024/day6.clj
+++ src/adventofcode/y2024/day6.clj
@@ -5,94 +5,55 @@
 
 (def input (get-input 2024 6))
 (def example (get-example 2024 6))
-(def keepers #{"#" "^"})
-(def guard #{"^" ">"})
-(def directions [:up :right :down :left])
 
 (defn build-grid [lines]
   (->> lines
        (map #(split % #""))
        to-array-2d))
 
-(defn get-nodes [grid]
-  (apply
-   concat
-   (map-indexed
-    (fn [y row]
-      (map-indexed
-       (fn [x val] [x y val])
-       row))
-    grid)))
+(defn apply-direction [[x y direction]]
+  (cond (= :up direction) [x (- y 1) :up]
+        (= :right direction) [(+ x 1) y :right]
+        (= :down direction) [x (+ y 1) :down]
+        (= :left direction) [(- x 1) y :left]))
 
-(defn areas-of-interest [nodes]
-  (filter
-   #(contains? keepers (nth % 2))
-   nodes))
+(defn rotate [[x y direction]]
+  (cond (= :up direction) [x y :right]
+        (= :right direction) [x y :down]
+        (= :down direction) [x y :left]
+        (= :left direction) [x y :up]))
 
-(defn dpreds [direction x y]
-  (cond
-    (= direction :up) (fn [node] (and (= x (nth node 0)) (> y (nth node 1))))
-    (= direction :right) (fn [node] (and (= y (nth node 1)) (< x (nth node 0))))
-    (= direction :down) (fn [node] (and (= x (nth node 0)) (< y (nth node 1))))
-    (= direction :left) (fn [node] (and (= y (nth node 1)) (> x (nth node 0))))))
+(defn safe-aget [[x y _direction] grid]
+  (if (and (> x -1)
+           (> y -1)
+           (< y (count grid))
+           (< x (count (nth grid 0))))
+    (aget grid y x)
+    :out-of-bounds))
 
-(defn dvecs [direction]
-  (cond
-    (= direction :up) [0 1]
-    (= direction :right) [-1 0]
-    (= direction :down) [0 -1]
-    (= direction :left) [1 0]))
+(defn walk [[x y direction] grid steps]
+  (let [current (safe-aget [x y direction] grid)
+        next (safe-aget (apply-direction [x y direction]) grid)]
+    (if (= :out-of-bounds current)
+      steps
+      (if (= "#" next)
+        (recur
+         (rotate
+          [x y direction]) grid steps)
+        (recur
+         (apply-direction
+          [x y direction]) grid (conj steps [x y]))))))
 
-(defn distance-comp [gx gy]
-  (fn [[x1 y1] [x2 y2]]
-    (- (+ (abs (- gx x1)) (abs (- gy y1)))
-       (+ (abs (- gx x2)) (abs (- gy y2))))))
-
-(defn closest [gx gy nodes]
-  (sort-by #((nth % 0) (nth % 1)) (distance-comp gx gy) nodes))
-
-(defn find-guard [nodes]
+(defn find-guard [grid]
   (first
-   (filter
-    #(guard (nth % 2))
-    nodes)))
-
-(defn find-next [direction gx gy nodes]
-  (map +
-       (dvecs direction)
-       (first
-        (closest
-         gx
-         gy
-         (filter
-          (dpreds direction gx gy)
-          nodes)))))
-
-(defn guard-walk [nodes]
-  (loop [route []
-         direction 0
-         guard (find-guard nodes)]
-    (if (empty? guard)
-      route
-      (recur (cons guard route)
-             (+ direction 1)
-             (find-next
-              (nth directions (mod direction 4))
-              (nth guard 0)
-              (nth guard 1)
-              nodes)))))
-
-(defn diffs [[a b]] 
-  (abs 
-   (- (+ (nth a 0) (nth a 1)) 
-      (+ (nth b 0) (nth b 1)))))
+   (for [[y row] (map-indexed vector grid)
+         [x val] (map-indexed vector row)
+         :when (= "^" val)]
+     [x y :up])))
 
 (defn part1 [data]
-  (->> data
-       build-grid
-       get-nodes
-       areas-of-interest
-       guard-walk
-       (partition 2 1)))
+  (let [grid (build-grid data)
+        guard (find-guard grid)]
+    (distinct (walk guard grid []))))
 
-(part1 example)
+(count (part1 input))
